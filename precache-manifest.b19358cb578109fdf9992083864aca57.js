6028b34cda0c783fcc3a02612d6f9ff572e6622f
Tijana <Tijana>
2020-10-01 12:18:04 +0200

    Updates

---
+++ precache-manifest.b19358cb578109fdf9992083864aca57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46344e6a205188992fc6fb1e80a92bf7",
+    "url": "/designYourCake/index.html"
+  },
+  {
+    "revision": "43c973a6f387168643c3",
+    "url": "/designYourCake/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6cab68565f430a1aaa8",
+    "url": "/designYourCake/static/js/2.fd6db3ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/designYourCake/static/js/2.fd6db3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c973a6f387168643c3",
+    "url": "/designYourCake/static/js/main.9c22223f.chunk.js"
+  },
+  {
+    "revision": "3dfae4cdfed2de072bc4",
+    "url": "/designYourCake/static/js/runtime-main.2363d87f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/designYourCake/static/media/logo.5d5d9eef.svg"
+  }
+]);
